2fea2cc038a13e633dd7b68398646c203c52a27e
Gigi M <mmgamao1@gmail.com>
2019-08-12 19:34:45 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,14 +1,3 @@
 class Model
-attr_accessor :name, :breed, :age
-  DOGS = []
-  def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
-    DOGS << self
-  end
 
-  def self.all
-    DOGS
-  end
 end
